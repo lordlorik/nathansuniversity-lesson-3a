724f5b0fdc7fccae9d800d9fccdb5bb56e7f5706
Lord LoriK <lord.lorik@gmail.com>
2012-04-28 16:18:28 -0300

    First commit

---
+++ parser.js
@@ -0,0 +1,23 @@
+var PEG = require('pegjs');
+var assert = require('assert');
+var fs = require('fs'); // for loading files
+
+// Read file contents
+var data = fs.readFileSync('scheem.peg', 'utf-8');
+
+// Show the PEG grammar file
+console.log(data);
+
+// Create my parser
+var parse = PEG.buildParser(data).parse;
+
+// Do a test
+assert.deepEqual(parse('a'), 'a');
+assert.deepEqual(parse('(a)'), ['a']);
+assert.deepEqual(parse('(a b c)'), ['a', 'b', 'c']);
+assert.deepEqual(parse(' (a  b  c) '), ['a', 'b', 'c']);
+assert.deepEqual(parse('\'a'), ['quote', 'a']);
+assert.deepEqual(parse('\'(a b c)'), ['quote', ['a', 'b', 'c']]);
+assert.deepEqual(parse('(a b c) ;; comment with closing parenthesis )'), ['a', 'b', 'c']);
+assert.deepEqual(parse('(a b c ;; comment with closing parenthesis )\n) '), ['a', 'b', 'c']);
+assert.deepEqual(parse(';; comment only'), '');
